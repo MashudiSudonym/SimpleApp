327de1240641b257a4cc9d0e932296f298ea9477
Muhamad Mashudi Ardi Winata <muhamadmashudiardiwinata@gmail.com>
2022-06-24 22:55:29 +0700

    configuration Splash Screen SplashScreenActivity.kt

---
+++ app/src/main/java/c/m/simpleapp/SimpleApp.kt
@@ -0,0 +1,17 @@
+package c.m.simpleapp
+
+import android.app.Application
+import com.ramcosta.composedestinations.BuildConfig
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class SimpleApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
---
+++ app/src/main/java/c/m/simpleapp/splash_screen/SplashScreenActivity.kt
@@ -0,0 +1,33 @@
+package c.m.simpleapp.splash_screen
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
+import c.m.simpleapp.MainActivity
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+
+@AndroidEntryPoint
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val splashScreen = installSplashScreen()
+            splashScreen.setKeepOnScreenCondition { true }
+        }
+
+        super.onCreate(savedInstanceState)
+
+        lifecycleScope.launchWhenCreated {
+            delay(3000)
+
+            val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
