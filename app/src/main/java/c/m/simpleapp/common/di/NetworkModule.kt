863382a432941db6bb49917fabc5f0d2f9bc9fa1
Muhamad Mashudi Ardi Winata <muhamadmashudiardiwinata@gmail.com>
2022-06-25 14:38:59 +0700

    initialize module dependency injection for network

---
+++ app/src/main/java/c/m/simpleapp/common/di/NetworkModule.kt
@@ -0,0 +1,41 @@
+package c.m.simpleapp.common.di
+
+import c.m.simpleapp.common.util.Constants
+import com.ramcosta.composedestinations.BuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    @Provides
+    fun provideBaseUrlApi(): String = Constants.BASE_URL_API
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient = if (BuildConfig.DEBUG) {
+        val loggingInterceptor = HttpLoggingInterceptor()
+            .setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        OkHttpClient.Builder().addInterceptor(loggingInterceptor).build()
+    } else {
+        OkHttpClient.Builder().build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofitBuilder(okHttpClient: OkHttpClient, baseUrlApi: String): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(baseUrlApi)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+    }
+}
