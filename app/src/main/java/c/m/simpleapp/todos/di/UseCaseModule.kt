f8e45ca81cd108086ed7ca658b1236b6ecc4472a
Muhamad Mashudi Ardi Winata <muhamadmashudiardiwinata@gmail.com>
2022-06-25 19:26:15 +0700

    rename file use case layer

--- app/src/main/java/c/m/simpleapp/todos/di/UseCaseModule.kt
+++ app/src/main/java/c/m/simpleapp/todos/di/UseCaseModule.kt
@@ -1,8 +1,8 @@
 package c.m.simpleapp.todos.di
 
 import c.m.simpleapp.todos.domain.repository.TodosRepository
-import c.m.simpleapp.todos.domain.use_case.get_todo_detail.GetTodoDetail
-import c.m.simpleapp.todos.domain.use_case.get_todos.GetTodos
+import c.m.simpleapp.todos.domain.use_case.get_todo_detail_use_case.GetTodoDetailUseCase
+import c.m.simpleapp.todos.domain.use_case.get_todos_use_case.GetTodosUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -14,13 +14,13 @@ import javax.inject.Singleton
 object UseCaseModule {
     @Provides
     @Singleton
-    fun provideGetTodos(todosRepository: TodosRepository): GetTodos {
-        return GetTodos(todosRepository)
+    fun provideGetTodosUseCase(todosRepository: TodosRepository): GetTodosUseCase {
+        return GetTodosUseCase(todosRepository)
     }
 
     @Provides
     @Singleton
-    fun provideGetTodoDetail(todosRepository: TodosRepository): GetTodoDetail {
-        return GetTodoDetail(todosRepository)
+    fun provideGetTodoDetailUseCase(todosRepository: TodosRepository): GetTodoDetailUseCase {
+        return GetTodoDetailUseCase(todosRepository)
     }
 }
