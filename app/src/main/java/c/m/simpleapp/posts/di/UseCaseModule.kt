6c93713b213fe7842bd6662ee77c23ebd9b0b15d
Muhamad Mashudi Ardi Winata <muhamadmashudiardiwinata@gmail.com>
2022-07-07 15:07:37 +0700

    rename get post use case to get post detail use case

--- app/src/main/java/c/m/simpleapp/posts/di/UseCaseModule.kt
+++ app/src/main/java/c/m/simpleapp/posts/di/UseCaseModule.kt
@@ -2,7 +2,7 @@ package c.m.simpleapp.posts.di
 
 import c.m.simpleapp.posts.domain.repository.PostsRepository
 import c.m.simpleapp.posts.domain.use_case.get_list_posts_use_case.GetListPostsUseCase
-import c.m.simpleapp.posts.domain.use_case.get_post_use_case.GetPostUseCase
+import c.m.simpleapp.posts.domain.use_case.get_post_detail_use_case.GetPostDetailUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -20,7 +20,7 @@ object UseCaseModule {
 
     @Provides
     @Singleton
-    fun provideGetPostUseCase(postsRepository: PostsRepository): GetPostUseCase {
-        return GetPostUseCase(postsRepository)
+    fun provideGetPostDetailUseCase(postsRepository: PostsRepository): GetPostDetailUseCase {
+        return GetPostDetailUseCase(postsRepository)
     }
 }
--- app/src/main/java/c/m/simpleapp/posts/domain/use_case/get_post_detail_use_case/GetPostDetailUseCase.kt
+++ app/src/main/java/c/m/simpleapp/posts/domain/use_case/get_post_detail_use_case/GetPostDetailUseCase.kt
@@ -1,11 +1,11 @@
-package c.m.simpleapp.posts.domain.use_case.get_post_use_case
+package c.m.simpleapp.posts.domain.use_case.get_post_detail_use_case
 
 import c.m.simpleapp.common.util.Resource
 import c.m.simpleapp.posts.domain.model.Post
 import c.m.simpleapp.posts.domain.repository.PostsRepository
 import kotlinx.coroutines.flow.Flow
 
-class GetPostUseCase(private val postsRepository: PostsRepository) {
+class GetPostDetailUseCase(private val postsRepository: PostsRepository) {
     suspend operator fun invoke(postId: Int): Flow<Resource<Post>> {
         return postsRepository.getPost(postId)
     }
