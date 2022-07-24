92bfcaf8a616482dfef0e41f277a4e3642b133c4
Muhamad Mashudi Ardi Winata <muhamadmashudiardiwinata@gmail.com>
2022-07-24 23:44:21 +0700

    remove accompanist inset

--- app/src/main/java/c/m/simpleapp/common/presentation/MainActivity.kt
+++ app/src/main/java/c/m/simpleapp/common/presentation/MainActivity.kt
@@ -6,36 +6,23 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import c.m.simpleapp.common.presentation.ui.theme.SimpleAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             SimpleAppTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SimpleAppTheme {
-        Greeting("Android")
     }
 }
