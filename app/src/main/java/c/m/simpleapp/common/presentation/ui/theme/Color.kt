864f18d5f0c0d81661a9799e7af2fc03a23deee9
Muhamad Mashudi Ardi Winata <muhamadmashudiardiwinata@gmail.com>
2022-08-13 00:25:07 +0700

    add custom font

--- app/src/main/java/c/m/simpleapp/common/presentation/ui/theme/Color.kt
+++ app/src/main/java/c/m/simpleapp/common/presentation/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package c.m.simpleapp.common.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Green200 = Color(0xFFA5D6A7)
-val Green500 = Color(0xFF4CAF50)
-val Green700 = Color(0xFF388E3C)
-val Yellow200 = Color(0xFFFFF59D)
+//val Green200 = Color(0xFFA5D6A7)
+//val Green500 = Color(0xFF4CAF50)
+//val Green700 = Color(0xFF388E3C)
+//val Yellow200 = Color(0xFFFFF59D)
 val FirstColor = Color(0xFFFAF7ED)
 val FirstVariantColor = Color(0xFFFDEDB6)
 val SecondColor = Color(0xFFB3DDC6)
