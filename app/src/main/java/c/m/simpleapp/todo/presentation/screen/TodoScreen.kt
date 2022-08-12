0b58f0233931daa9e91c28123393e9c5ad5447de
Muhamad Mashudi Ardi Winata <muhamadmashudiardiwinata@gmail.com>
2022-08-12 22:24:18 +0700

    dummy list for todo page

--- app/src/main/java/c/m/simpleapp/todo/presentation/screen/TodoScreen.kt
+++ app/src/main/java/c/m/simpleapp/todo/presentation/screen/TodoScreen.kt
@@ -1,11 +1,14 @@
 package c.m.simpleapp.todo.presentation.screen
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
 import c.m.simpleapp.common.presentation.component.custom.AppBarCustom
 import c.m.simpleapp.common.presentation.component.custom.BottomNavigationContentWrapperCustom
 import com.ramcosta.composedestinations.annotation.Destination
@@ -22,8 +25,24 @@ fun TodoScreen(navigator: DestinationsNavigator) {
                 AppBarCustom(title = "Todo")
             },
         ) { padding ->
-            Column(modifier = Modifier.padding(padding)) {
-                Text(text = "hello")
+            LazyColumn(
+                modifier = Modifier
+                    .padding(
+                        start = padding.calculateStartPadding(
+                            layoutDirection = LayoutDirection.Ltr),
+                        end = padding.calculateEndPadding(layoutDirection = LayoutDirection.Ltr),
+                        top = padding.calculateTopPadding(),
+                        bottom = padding.calculateBottomPadding()
+                    )
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceBetween
+            ) {
+                item {
+                    for (i in 1..100) {
+                        Text("Hello $i", modifier = Modifier.padding(16.dp))
+                    }
+                }
             }
         }
     }
