2d8c64422fa9cd24f6ac90bc05caeee666f69e43
Muhamad Mashudi Ardi Winata <muhamadmashudiardiwinata@gmail.com>
2022-08-13 00:50:33 +0700

    add root screen preview function

--- app/src/main/java/c/m/simpleapp/common/presentation/screen/RootScreen.kt
+++ app/src/main/java/c/m/simpleapp/common/presentation/screen/RootScreen.kt
@@ -2,10 +2,13 @@ package c.m.simpleapp.common.presentation.screen
 
 import android.annotation.SuppressLint
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import c.m.simpleapp.NavGraphs
 import c.m.simpleapp.common.presentation.component.bottom_navigation_bar.BottomNavigationBar
+import c.m.simpleapp.common.presentation.ui.theme.SimpleAppTheme
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.annotation.Destination
 
@@ -20,5 +23,15 @@ fun RootScreen() {
             BottomNavigationBar(navController = navController)
         }) {
         DestinationsNavHost(navController = navController, navGraph = NavGraphs.root)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun RootScreenPreview() {
+    SimpleAppTheme {
+        Surface {
+            RootScreen()
+        }
     }
 }
