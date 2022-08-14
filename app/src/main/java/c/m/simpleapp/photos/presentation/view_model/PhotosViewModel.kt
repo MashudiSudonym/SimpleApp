006ed900eab33945d4e51313bf86c4cf74dc9bdc
Muhamad Mashudi Ardi Winata <muhamadmashudiardiwinata@gmail.com>
2022-08-14 15:57:54 +0700

    add hilt view model annotation

--- app/src/main/java/c/m/simpleapp/photos/presentation/view_model/PhotosViewModel.kt
+++ app/src/main/java/c/m/simpleapp/photos/presentation/view_model/PhotosViewModel.kt
@@ -3,8 +3,10 @@ package c.m.simpleapp.photos.presentation.view_model
 import androidx.lifecycle.ViewModel
 import c.m.simpleapp.photos.domain.use_case.get_list_photos_use_case.GetListPhotosUseCase
 import c.m.simpleapp.photos.domain.use_case.get_photo_detail_use_case.GetPhotoDetailUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+@HiltViewModel
 class PhotosViewModel @Inject constructor(
     private val getListPhotosUseCase: GetListPhotosUseCase,
     private val getPhotoDetailUseCase: GetPhotoDetailUseCase,
