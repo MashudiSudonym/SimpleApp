b2be253a2e524befcafbb601bfcc5586c41ba96f
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2022-08-26 20:30:08 +0700

    change text color

--- app/src/main/java/c/m/simpleapp/common/presentation/component/custom/LocalContentBanner.kt
+++ app/src/main/java/c/m/simpleapp/common/presentation/component/custom/LocalContentBanner.kt
@@ -9,8 +9,11 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import c.m.simpleapp.R
 import c.m.simpleapp.common.presentation.ui.theme.SimpleAppTheme
 
 @Composable
@@ -18,8 +21,10 @@ fun LocalContentBanner(modifier: Modifier) {
     Box(modifier = modifier) {
         Text(
             modifier = Modifier.padding(16.dp),
-            text = "This data from local content",
+            text = stringResource(R.string.local_content_banner_text),
             style = MaterialTheme.typography.caption,
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colors.primary,
         )
     }
 }
@@ -29,9 +34,11 @@ fun LocalContentBanner(modifier: Modifier) {
 private fun LocalContentAlertPreview() {
     SimpleAppTheme {
         Surface {
-            LocalContentBanner(modifier = Modifier
-                .fillMaxWidth()
-                .background(color = MaterialTheme.colors.secondary))
+            LocalContentBanner(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(color = MaterialTheme.colors.secondary)
+            )
         }
     }
 }
