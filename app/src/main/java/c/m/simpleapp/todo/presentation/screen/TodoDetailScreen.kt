c64dcee4e38e4f07ddf76bb02d9f24dedfe74f9d
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2022-08-26 13:54:24 +0700

    add todo detail screen

---
+++ app/src/main/java/c/m/simpleapp/todo/presentation/screen/TodoDetailScreen.kt
@@ -0,0 +1,10 @@
+package c.m.simpleapp.todo.presentation.screen
+
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+
+@Destination
+@Composable
+fun TodoDetailScreen() {
+
+}
